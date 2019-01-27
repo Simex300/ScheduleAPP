b02df4f6a8fd260189bfc8a5dc5e446753029b50
PC\Adonis <adonis0444@gmail.com>
2019-01-26 22:17:49 -0500

    Adding grid to the library component

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import libraryCourse from './libraryCoruse';
+import LibraryCourse from './libraryCoruse';
+
 class Library extends Component {
     render() {
         return (
             <div className="library">
                 <h1 className="library__title">Course Library</h1>
+                <LibraryCourse />
+                <LibraryCourse />
+                <LibraryCourse />
             </div>
         );
     }
---
+++ src/components/library/libraryCoruse.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library-course__title">Problem Solving</label>
+                {/* Icon Component */}
+                {/* Arrow Component */}
+                {/* Action Btn Component */}
+                <div className="library-course__description">
+                    <label>Course Description</label>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus interdum dignissim augue bibendum pulvinar. Quisque pellentesque nisl est, a mollis risus bibendum in. Vivamus condimentum, nulla nec accumsan facilisis, est nisl iaculis erat, ac congue ex velit nec nisi. Phasellus porttitor dolor tortor, sit amet fringilla lorem lacinia sit amet. Proin ultrices lorem nec cursus lobortis. In ac volutpat lorem. Suspendisse tempus felis sed velit semper, et sagittis ante mattis. Ut non lorem sed ex accumsan luctus id ac arcu. Nunc at neque augue. In non ultricies felis.</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LibraryCourse;
