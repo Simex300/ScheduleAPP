0a364f4ae73f5cfaa25f5770e1829a7202fec6df
PC\Adonis <adonis0444@gmail.com>
2019-01-27 10:23:45 -0500

    Adding icons

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
-import libraryCourse from './libraryCoruse';
-import LibraryCourse from './libraryCoruse';
+import LibraryCourse from './libraryCourse';
 
 class Library extends Component {
     render() {
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Icon from '../Icon';
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
                 <label className="library-course__title">Problem Solving</label>
-                {/* Icon Component */}
+                { Icon("fas fa-check", "library-course__icon") }
                 {/* Arrow Component */}
                 {/* Action Btn Component */}
                 <div className="library-course__description">
