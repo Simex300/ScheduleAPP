39bcd580094d7c319bf77a1731f8a869595f619e
PC\Adonis <adonis0444@gmail.com>
2019-01-29 17:28:19 -0500

    Adding the tracking functionality

--- src/components/schedule/tracker.js
+++ src/components/schedule/tracker.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 class Tracker extends Component {
+    calculateProgress = function() {
+        return (this.props.amountEnrolled / this.props.amountPossible) * 100;
+    }.bind(this);
+
     render() {
         return(
             <div className='tracker'>
                 <label className='tracker__label'>Progress Tracker</label>
                 <div className='tracker__percentage'>
-                    0%
+                    {this.calculateProgress()}%
                 </div>
             </div>
         )
