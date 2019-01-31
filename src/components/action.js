31f5b8b8f5b6aeae53dce51356a39c4bebe72a2e
PC\Adonis <adonis0444@gmail.com>
2019-01-30 20:29:27 -0500

    Adding animation of the add/remove buttom

--- src/components/action.js
+++ src/components/action.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import Icon from './icon';
-
-// Icons
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPlusCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
-
-library.add( faPlusCircle, faTimesCircle );
 
 class Action extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.status = false;
+    }
+
+    handleAction = function(){
+        this.props.onClick();
+        if(!this.status){
+            document.getElementById(this.id).classList.add('action-remove');
+        }
+        else{
+            document.getElementById(this.id).classList.remove('action-remove');
+        }
+        this.status = !this.status;
+    }.bind(this);
+
     render(){
+        this.id = `action-${this.props.id}`;
         return (
-            <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-                { Icon("fas fa-plus-circle") }
-            </button>
+            <a id={this.id} onClick={() => this.handleAction()} className={`${this.props.className} action`}>
+                
+            </a>
         )
     }
 }
